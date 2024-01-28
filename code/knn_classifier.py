a48d6e8c594afd0f63932e4f2f4ca9478888202d
Davide236 <rigodavi99@gmail.com>
2024-01-28 17:12:40 +0100

    Adding N-fold cross validation

--- code/knn_classifier.py
+++ code/knn_classifier.py
@@ -7,6 +7,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.model_selection import GridSearchCV
 from dataset import CBISDDSM
 from sklearn.metrics import confusion_matrix
+import numpy as np
 
 
 def get_features(loader):
@@ -19,6 +20,7 @@ def get_features(loader):
     return features,labels
 
 def knn_classifier(train_loader, test_loader, k):
+    print(f'Running KNN for k={k}...')
     # Extract features and labels from the training set
     train_features, train_labels = get_features(train_loader)
     # Extract features and labels from the test set
@@ -46,18 +48,47 @@ def knn_classifier(train_loader, test_loader, k):
     print(f'False Positives Rate: {fp_rate * 100:.2f}%')
 
 
+def manual_n_fold_cross_validation(data, n_folds):
+    total_samples = len(data)
+    shuffled_array = create_shuffled_array(total_samples-1)
+
+    folds = [[] for _ in range(n_folds)]  # Initialize empty lists for each fold
+    count = 0
+    for i, item in enumerate(shuffled_array):
+        if count >= n_folds:
+            count = 0
+        folds[count].append(data[item])
+        count += 1
+
+    return folds
+
+def create_shuffled_array(size):
+    original_array = np.arange(1, size + 1)  # Creates an array from 1 to size
+    shuffled_array = np.random.permutation(original_array)
+    return shuffled_array
+
+
+def get_train_test_sets(folds, fold_index):
+    # Combine all folds except the test fold to create training data
+    train_data = [data for i, fold in enumerate(folds) if i != fold_index for data in fold]
+    # Use the test fold as test data
+    test_data = folds[fold_index]
+
+    return train_data, test_data
+
 if __name__ == '__main__':
     path = "../"
-    param_grid = {'n_neighbors': [3, 5, 7, 9, 11, 13]}
+    n_neighbors = [3, 5, 7, 9, 11]
 
-
+    n_folds = 5
     # Split the dataset into train and test sets
     data = CBISDDSM(file="train2.csv", path=path)
-    # 80% training, 20% testing
-    train_data, test_data = train_test_split(data, test_size=0.2, random_state=42)
-    # Create data loaders
-    train_loader = DataLoader(train_data, batch_size=16, shuffle=True, num_workers=4)
-    test_loader = DataLoader(test_data, batch_size=16, shuffle=False, num_workers=4)
+    folds = manual_n_fold_cross_validation(data, n_folds)
 
-    # Run KNN classifier
-    knn_classifier(train_loader, test_loader, k=3)
+    for i in range(n_folds):
+        train_data, test_data = get_train_test_sets(folds, i)
+        # Create data loaders
+        train_loader = DataLoader(train_data, batch_size=16, shuffle=True, num_workers=4)
+        test_loader = DataLoader(test_data, batch_size=16, shuffle=False, num_workers=4)
+        # Run KNN classifier
+        knn_classifier(train_loader, test_loader, n_neighbors[i])
