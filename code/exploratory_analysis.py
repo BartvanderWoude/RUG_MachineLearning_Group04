416beb1451397a2d85e9a697f38320a84e73a34c
somak-chatterjee <chatterjeesomak7@gmail.com>
2024-01-29 11:59:20 +0100

    Dataset visualization

--- code/exploratory_analysis.py
+++ code/exploratory_analysis.py
@@ -1,47 +1,33 @@
 import os
-import random
 import cv2
 import matplotlib.pyplot as plt
+import random
 
-# Set the path to your dataset directory
-dataset_path = "../CBIS-DDSM/jpeg/CBIS-DDSM/jpeg/1.3.6.1.4.1.9590.100.1.2.126082211045731020508108042042916052/"
+class DatasetVisualizer:
+    def __init__(self, dataset_path):
+        self.dataset_path = dataset_path
+        self.image_files = [f for f in os.listdir(dataset_path) if f.endswith(".jpg") or f.endswith(".jpeg")]
 
-# Function to load and display sample images
-def display_sample_images(dataset_path, num_samples=5):
-    # Get a list of image files in the dataset directory
-    image_files = [f for f in os.listdir(dataset_path) if f.endswith(".jpg") or f.endswith(".jpeg")]
+    def visualize_dataset(self, num_samples=5):
+        # Randomly choose samples
+        sample_files = random.sample(self.image_files, num_samples)
 
-    # Display a random sample of images
-    for _ in range(num_samples):
-        # Choose a random image file
-        image_file = random.choice(image_files)
+        # Set up subplots
+        fig, axes = plt.subplots(1, num_samples, figsize=(15, 3))
 
-        # Load the image using OpenCV
-        image_path = os.path.join(dataset_path, image_file)
-        image = cv2.imread(image_path)
+        # Display sample images
+        for i, image_file in enumerate(sample_files):
+            image_path = os.path.join(self.dataset_path, image_file)
+            image = cv2.imread(image_path)
 
-        # Display the image using Matplotlib
-        plt.imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
-        plt.title(image_file)
+            # Convert BGR to RGB for displaying with Matplotlib
+            image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+            # Display the image
+            axes[i].imshow(image_rgb)
+            axes[i].set_title(image_file)
+            axes[i].axis("off")
+
         plt.show()
 
-# Function to get basic information about the images
-def inspect_images(dataset_path):
-    image_files = [f for f in os.listdir(dataset_path) if f.endswith(".jpg") or f.endswith(".jpeg")]
 
-    for image_file in image_files:
-        image_path = os.path.join(dataset_path, image_file)
-        image = cv2.imread(image_path)
-
-        # Print basic information
-        print(f"Image: {image_file}")
-        print(f"Shape: {image.shape}")
-        print(f"Minimum pixel value: {image.min()}")
-        print(f"Maximum pixel value: {image.max()}")
-        print()
-
-# Display sample images
-display_sample_images(dataset_path)
-
-# Inspect basic information about the images
-inspect_images(dataset_path)
